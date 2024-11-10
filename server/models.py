de33f23738bf013e9793c3ed432f1e0b35285208
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-11-10 12:45:35 +0300

    Update models

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Text, DateTime, Enum, ForeignKey, DECIMAL
+from sqlalchemy import Column, String, Table, Text, DateTime, Enum, ForeignKey, DECIMAL
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy_serializer import SerializerMixin
@@ -18,9 +18,115 @@ class User(db.Model, SerializerMixin):
     email = Column(String, unique=True)
     password = Column(String)
     role = Column(Enum(RoleEnum), nullable=False)
+    token = Column(Text, default='')
 
     jobseeker = relationship("JobSeeker", uselist=False, back_populates="user")
     employer = relationship("Employer", uselist=False, back_populates="user")
+
+# Employer stories: CRUD jobs, make payments and shortlist applications
+class Employer(db.Model, SerializerMixin):
+    __tablename__ = "employers"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), unique=True)
+    name = Column(String)
+    location = Column(String)
+    description = Column(String)
+    mission = Column(String)
+    vision = Column(String)
+
+    user = relationship("User", back_populates="employer")
+    jobs = relationship("Job", back_populates="employer")
+    payment = relationship("Payment", back_populates="employer")
+    serialize_rules = ("-user.employer", "-payment.employer", "-jobs.employer")
+
+class JobTypeEnum(enum.Enum):
+    freelance = "freelance"
+    fulltime = "full_time"
+    parttime = "part_time"
+    internship = "internship"
+
+class JobLevelEnum(enum.Enum):
+    entry_level = "entry_level"
+    mid_level = "mid_level"
+    senior_level = "senior_level"
+
+class IndustryEnum(enum.Enum):
+    telcos = "telcos"
+    software = "software"
+
+# Association table for short-listed applications
+shortlisted_applications = Table(
+    'shortlisted_applications',
+    db.Model.metadata,
+    Column('job_id', UUID(as_uuid=True), ForeignKey('jobs.id'), primary_key=True),
+    Column('application_id', UUID(as_uuid=True), ForeignKey('applications.id'), primary_key=True)
+)
+
+class Job(db.Model, SerializerMixin):
+    __tablename__ = "jobs"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    employer_id = Column(UUID(as_uuid=True), ForeignKey("employers.id"))
+    industry = Column(Enum(IndustryEnum))
+    title = Column(String)
+    description = Column(Text)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    level = Column(Enum(JobLevelEnum))
+    job_type = Column(Enum(JobTypeEnum))
+    date_posted = Column(DateTime)
+
+    job_responsibilities = relationship("JobResponsibility", back_populates="job")
+    job_requirements = relationship("Requirement", back_populates="job")
+    employer = relationship("Employer", back_populates="jobs")
+    shortlisted_applications = relationship("Application", secondary=shortlisted_applications, back_populates="shortlisted_jobs")
+    serialize_rules = ("-job_responsibilities.job", "-job_requirements.job",)
+
+class JobRequirement(db.Model, SerializerMixin):
+    __tablename__ = "job_requirements"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    job_id = Column(UUID(as_uuid=True), ForeignKey("jobs.id"))
+    requirements = Column(Text)
+
+    job = relationship("Job", back_populates="requirements")
+    serialize_rules = ("-job.job_requirements",)
+
+class JobResponsibility(db.Model, SerializerMixin):
+    __tablename__ = "job_responsibilities"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    description = Column(Text)
+    job_id = Column(UUID(as_uuid=True), ForeignKey("jobs.id"))
+
+    job = relationship("Job", back_populates="responsibilities")
+    serialize_rules = ("-job.job_responsibilities",)
+
+class PaymentTypeEnum(enum.Enum):
+    mpesa = "mpesa"
+    bank = "bank"
+
+class Payment(db.Model, SerializerMixin):
+    __tablename__ = "payments"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    employer_id = Column(UUID(as_uuid=True), ForeignKey("employers.id"))
+    plan_id = Column(UUID(as_uuid=True), ForeignKey("plans.id"))
+    payment_type = Column(Enum(PaymentTypeEnum))
+
+    plan = relationship("Plan", back_populates="payment")
+    employer = relationship("Employer", back_populates="payment")
+    serialize_rules = ("-plan.payment", "-employer.payment",)
+
+class PlanEnum(enum.Enum):
+    freemium = "freemium"
+    premium = "premium"
+    pro_rated = "pro_rated"
+
+class Plan(db.Model, SerializerMixin):
+    __tablename__ = "plans"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(Enum(PlanEnum))
+    price = Column(DECIMAL)
+
+    payment = relationship("Payment", back_populates="plan")
+    serialize_rules = ("-payments.plan",)
 
 # Jobseeker stories: Applying for jobs, Editing data including education and experience
 class JobSeeker(db.Model, SerializerMixin):
@@ -87,100 +193,5 @@ class Application(db.Model, SerializerMixin):
     status = Column(Enum(ApplicationStatusEnum), default=ApplicationStatusEnum.pending)
 
     jobseeker = relationship("JobSeeker", back_populates="applications")
+    shortlisted_jobs = relationship("Job", secondary=shortlisted_applications, back_populates="shortlisted_applications")
     serialize_rules = ("-jobseeker.applications",)
-
-# Employer stories: CRUD jobs and make payments
-class Employer(db.Model, SerializerMixin):
-    __tablename__ = "employers"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), unique=True)
-    name = Column(String)
-    location = Column(String)
-    description = Column(String)
-    mission = Column(String)
-    vision = Column(String)
-
-    user = relationship("User", back_populates="employer")
-    jobs = relationship("Job", back_populates="employer")
-    payment = relationship("Payment", back_populates="employer")
-    serialize_rules = ("-user.employer", "-payment.employer", "-jobs.employer")
-
-class JobTypeEnum(enum.Enum):
-    freelance = "freelance"
-    fulltime = "full_time"
-    parttime = "part_time"
-    internship = "internship"
-
-class JobLevelEnum(enum.Enum):
-    entry_level = "entry_level"
-    mid_level = "mid_level"
-    senior_level = "senior_level"
-
-class IndustryEnum(enum.Enum):
-    telcos = "telcos"
-    software = "software"
-
-class Job(db.Model, SerializerMixin):
-    __tablename__ = "jobs"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    employer_id = Column(UUID(as_uuid=True), ForeignKey("employers.id"))
-    industry = Column(Enum(IndustryEnum))
-    title = Column(String)
-    description = Column(Text)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
-    level = Column(Enum(JobLevelEnum))
-    job_type = Column(Enum(JobTypeEnum))
-    date_posted = Column(DateTime)
-
-    job_responsibilities = relationship("JobResponsibility", back_populates="job")
-    job_requirements = relationship("Requirement", back_populates="job")
-    employer = relationship("Employer", back_populates="jobs")
-    serialize_rules = ("-job_responsibilities.job", "-job_requirements.job",)
-
-class JobRequirement(db.Model, SerializerMixin):
-    __tablename__ = "job_requirements"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    job_id = Column(UUID(as_uuid=True), ForeignKey("jobs.id"))
-    description = Column(Text)
-
-    job = relationship("Job", back_populates="requirements")
-    serialize_rules = ("-job.job_requirements",)
-
-class JobResponsibility(db.Model, SerializerMixin):
-    __tablename__ = "job_responsibilities"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    description = Column(Text)
-    job_id = Column(UUID(as_uuid=True), ForeignKey("jobs.id"))
-
-    job = relationship("Job", back_populates="responsibilities")
-    serialize_rules = ("-job.job_responsibilities",)
-
-class PaymentTypeEnum(enum.Enum):
-    mpesa = "mpesa"
-    bank = "bank"
-
-class Payment(db.Model, SerializerMixin):
-    __tablename__ = "payments"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    employer_id = Column(UUID(as_uuid=True), ForeignKey("employers.id"))
-    plan_id = Column(UUID(as_uuid=True), ForeignKey("plans.id"))
-    payment_type = Column(Enum(PaymentTypeEnum))
-
-    plan = relationship("Plan", back_populates="payment")
-    employer = relationship("Employer", back_populates="payment")
-    serialize_rules = ("-plan.payment", "-employer.payment",)
-
-class PlanEnum(enum.Enum):
-    freemium = "freemium"
-    premium = "premium"
-    pro_rated = "pro_rated"
-
-class Plan(db.Model, SerializerMixin):
-    __tablename__ = "plans"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = Column(Enum(PlanEnum))
-    price = Column(DECIMAL)
-
-    payment = relationship("Payment", back_populates="plan")
-    serialize_rules = ("-payments.plan",)
---
+++ server/routes/temp_app.py
@@ -0,0 +1,468 @@
+from server.models import (db,Company, Plan, Payment,User,PaymentTypeEnum,PlanEnum,Profile,Role,
+                    RoleEnum,UserRole,IndustryEnum,Industry,JobLevelEnum,JobResponsibility,JobTag,
+                    ApplicationStatusEnum,EducationLevelEnum,IndustryCompany,Job,Tag,UserTag,Requirement,Application,Education,Experience,Shortlisting)
+from flask_migrate import Migrate
+from flask import Flask, request, make_response
+from flask_restful import Api, Resource
+# from flask_jwt_extended import jwt_required
+from flask_migrate import Migrate
+from flask_cors import CORS
+# from auth import auth_bp,jwt,allow
+from datetime import datetime,timezone,timedelta
+
+app=Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+db.init_app(app)
+# jwt.init_app(app)
+api=Api(app)
+migrate=Migrate(app,db)
+
+# API endpoint
+@app.route('/')
+def index():
+    return '<h1>Project Server</h1>'
+
+###################################################################### USER RESOURCE ############################################################################################
+class Users(Resource):
+    
+    def get(self):
+        users=[]
+        for user in User.query.all():
+            response_body={
+                "id":user.id,
+                "email":user.email,
+                "code":user.code,
+                "profile":user.profile,
+                "role":user.roles
+            }
+            users.append(response_body)
+        
+        return make_response(users,200)
+    
+    #TO BE REVISTED DONT DELETE
+    # def post(self):
+        data=request.get_json()
+        email=data['email']
+        password=data['password']
+        role_name=data['role']
+        
+        errors=[]
+        
+        role=Role.query.filter_by(name=role_name).first()
+        
+        if not "@" in email or not email:
+            errors.append('email is required')
+        if len(password) <8 :
+            errors.append('Password should be at least 8 characters')
+        
+        user=User.get_user_by_email(email=email)
+        
+        if user is not None:
+            errors.append('A user with that email exists')
+        
+        if errors:
+            return make_response({
+                "errors":errors
+            },400)
+        
+        new_user=User(email=email)
+        new_user.set_password(password)
+        new_user.save()
+        
+        user_role=UserRole(user_id=new_user.id,role_id=role)
+        db.session.add(user_role)
+        db.session.commit()
+        
+        return make_response({
+            "message":"User successfully created",
+        },201)
+
+api.add_resource(Users,'/users')
+        
+class User_by_id(Resource):
+    def get(self,id):
+        user=User.query.filter_by(id=id).first()
+        
+        if not user:
+            return make_response({
+                "message":"No user found"
+            },400)
+        
+        return make_response(user.to_dict(),200)
+    
+    def delete(self,id):
+        user=User.query.filter_by(id=id).first()
+        if not user:
+            return make_response({
+                "message":"No user found"
+            },400)
+        
+        user.delete()
+        
+        return make_response({
+            "message":"user deleted successfully"
+        },200)
+    
+    def put(self,id):
+        user=user.query.filter_by(id=id).first()
+        if not user:
+            return make_response({
+            "message":"No user found"
+            },400)
+        
+        data=request.get_json()
+        
+        for key,value in data.items():
+            setattr(user,key,value)
+        
+        db.session.commit()
+        
+        return make_response({
+            "id":user.id,
+            "email":user.email,
+            "code":user.code,
+            "profile":user.profile,
+            "role":user.roles
+            
+        },200)    
+            
+            
+            
+api.add_resource(User_by_id,'/users/<int:id>')
+
+##################################################################COMPANY RESOURCE##############################################################################################
+class Companies(Resource):
+    #this route is used by the admin to get all the companies in the system
+    def get(self):
+        companies=[company.to_dict() for company in Company.query.all()]
+        if not companies:
+            return make_response({
+                "message":"No companies found"
+            },400)        
+        return make_response(companies,200)
+    
+    #this route is used by the admin to delete all the companies in the system
+    def delete(self):
+        companies=[company.to_dict() for company in Company.query.all()]
+        if not companies:
+            return make_response({
+                "message":"No companies found"
+            },400)
+
+        Company.query.delete()
+        return make_response({
+            "message":"Companies have been deleted"
+        },200)
+api.add_resource(Companies,'/companies')
+
+class Companies_by_id(Resource):
+    #this route is used by the admin to delete the company based on the id
+    def delete(self,id):
+        company=Company.query.filter_by(id=id).first()
+        
+        if not company:
+            return make_response({
+                "message":"No company is found"
+            })
+        
+        db.session.delete(company)
+        db.session.commit()
+        
+        return make_response({
+            "message":"company is successfully deleted"
+        })
+    
+    #This route is used by the employer when he/she is updating the company details
+    def put(self,id):
+        company=Company.query.filter_by(id=id).first()
+        if not company:
+            return make_response({
+                "message":"No company is found"
+            })
+        data=request.get_json()
+        
+        for key,value in data.items():
+            setattr(company,key,value)
+        
+        db.session.commit()
+        
+        return make_response(company.to_dict,200)
+    
+api.add_resource(Companies_by_id,'/companies/<int:id>')
+
+########################################################################### JOBS RESOURCE ######################################################################################
+class Jobs(Resource):
+    
+    # This route is used by both the admin and the job_seeker to view all the available jobs 
+    def get(self):
+        jobs=[job.to_dict for job in Job.query.all()]
+        
+        if not jobs:
+            return make_response({
+                "message":"No jobs found"
+            },400)
+        
+        return make_response(jobs,200)
+    
+    #This route is used by the employer to post a job
+    def post(self):
+        data=request.get_json()
+        title=data['title']
+        description=data['description']
+        level=data['level']
+        job_type=data['job_type']
+        salary=data['salary']
+        company_id=data['company_id']
+        
+        company=Company.query.filter_by(id=company_id).first()
+        
+        if not company:
+            return make_response({
+                "error":"Company with that company id is not found"
+            },400)
+        if not title or not isinstance(title, str):
+            return make_response({
+            "error": "Job title is required and should be a string"
+        }, 400)
+    
+        if not description or not isinstance(description, str):
+            return make_response({
+            "error": "Job description is required and should be a string"
+        }, 400)
+
+        if not level or level not in ['entry_level', 'mid_level', 'senior_level']:
+            return make_response({
+            "error": "Job level is required and should be one of: entry_level, mid_level, senior_level"
+        }, 400)
+
+        if not job_type or job_type not in ['freelance', 'fulltime', 'parttime', 'internship']:
+            return make_response({
+            "error": "Job type is required and should be one of: freelance, fulltime, parttime, internship"
+        }, 400)
+
+        if not salary or not isinstance(salary, (int, float)):
+            return make_response({
+            "error": "Job salary is required and should be a number"
+        }, 400)
+
+        job = Job(
+        title=title,
+        description=description,
+        level=level,
+        job_type=job_type,
+        salary=salary,
+        company_id=company_id
+        )
+        
+        db.session.add(job)
+        db.session.commit()
+        
+        # Add multiple responsibilities
+        for responsibility in data.get('responsibilities', []):
+            job_responsibility = JobResponsibility(description=responsibility, job_id=job.id)
+            db.session.add(job_responsibility)
+
+        # Add multiple requirements
+        for requirement in data.get('requirements', []):
+            job_requirement = Requirement(description=requirement, job_id=job.id)
+            db.session.add(job_requirement)
+        
+        db.session.commit()
+        
+        return make_response(job.to_dict(),201)
+    
+    #This route is used by the admin to delete all the jobs in the platform
+    def delete(self):
+        jobs=Job.query.all()
+        
+        if not jobs:
+            return make_response({
+                "error":"No jobs found"
+            },400)
+        
+        Job.query.delete()
+        
+        return make_response({
+            "error":"Jobs have been deleted successfully"
+        },200)
+
+api.add_resource(Jobs,'/jobs')
+
+class Jobs_by_id(Resource):
+    #this route is used by the employer to get all the jobs related to the employer's company
+    def get(self,id):
+        jobs=[job.to_dict() for job in Job.query.filter_by(company_id=id).all()]
+        
+        if not jobs:
+            return make_response({
+                "error":"No jobs have been found"
+            },400)
+            
+        return make_response(jobs,200)
+    
+    #this route is used by the employer or admin to delete a particular job
+    def delete(self,id):
+        job=Job.query.filter_by(id=id).first()
+        if not job:
+            return make_response({
+                "error":"No job is found"
+            },400)
+        db.session.delete(job)
+        db.session.commit()
+        
+        return make_response({
+            "message":"Job deleted successfuly"
+        })
+    #this route is used by the employer to edit a particular job
+    def put(self,id):
+        job=Job.query.filter_by(id=id).first()
+        
+        if not job:
+            return make_response({
+                "error":"No job is found"
+            },400)
+        
+        data=request.get_json()
+        for key,value in data:
+            setattr(job,key,value)         
+        job.update_at=datetime.now(timezone(timedelta(hours=3)))
+        db.session.commit()
+        
+        return make_response(job.to_dict(),200)
+
+api.add_resource(Jobs_by_id,'/jobs/<int:id>')
+
+class Jobs_by_company_id(Resource):
+    #this route is used by the employer to see all the jobs that belong to their company
+    #NOTE HERE WE ARE USING COMPANY ID
+    def get(self,id):
+        jobs=[job.to_dict() for job in Job.query.filter_by(company_id=id).all()]
+        
+        if not jobs:
+            return make_response(
+                {
+                    "error":"No jobs found"
+                },400
+            )
+        
+        return make_response(jobs,200)
+    
+api.add_resource(Jobs_by_company_id,'/jobs/company/<int:id>')        
+
+############################################################################### APPLICATION RESOURCE #################################################################################################
+class Applications(Resource):
+    #This route is used by the admin to see all the applications in the system
+    #Note we are going to use this route in the admin dashboard for metrics 
+    def get(self):
+        applications=[application.to_dict() for application in Application.query.all()]
+        if not applications:
+            return make_response({
+                "error":"Currently there are no applications"
+            },400)
+        
+        return make_response(applications,200)
+    
+    #This route is used by the job_seeker when he/she want to apply for a particular job
+    def post(self):
+        data=request.get_json()
+        user_id=data['user_id']
+        job_id=data['job_id']
+        status='pending'
+        
+        user=User.query.filter_by(id=user_id).first()
+        job=Job.query.filter_id(id=job_id).first()
+        
+        if not user:
+            return make_response(
+                {
+                    "error":"No user with that id is found"
+                },400
+            )
+        
+        if not job:
+            return make_response(
+                {
+                    "error":"No job with that id is found"
+                },400
+            )
+        
+        application=Application(user_id=user_id,job_id=job_id,status=status)
+        db.session.add(application)
+        db.session.commit()
+        
+        return make_response(application.to_dict(),200)
+                
+api.add_resource(Applications,'/applications')
+
+class Applications_by_id(Resource):
+    #This route is used by the employer
+    def get(self,id):
+        application=Application.query.filter_by(id=id).filter()
+        if not application:
+            return make_response({
+                "error":"There is no application with that id"
+            },400)
+            
+        return make_response(application.to_dict(),200)
+    
+    #This route is used by the job_seeker or the employer to delete an application
+    def delete(self,id):
+        application=Application.query.filter_by(id=id).filter()
+        if not application:
+            return make_response({
+                "error":"There is no application with that id"
+            },400)
+        
+        db.session.delete(application)
+        db.session.commit()
+        
+        return make_response(
+            {
+                "message":"application has been successfully deleted"
+            },400
+        )
+    #This route is used by the employer when he/she wants to update the status of the application 
+    def put(self,id):
+        application=Application.query.filter_by(id=id).filter()
+        if not application:
+            return make_response({
+                "error":"There is no application with that id"
+            },400)
+        
+        data=request.get_json()
+        
+        for key,value in data.items():
+            setattr(application,key,value)
+        
+        db.session.commit()
+        
+        return make_response(application.to_dict(),200)
+    
+api.add_resource(Applications_by_id,'/applications/id')
+
+class Applications_by_user_id(Resource):
+    #this route is used by the job_seeker to view all his/her application
+    #Note here we are using user_id ,this route is important in the job_seeker dashboard
+    def get(self,id):
+        applications=[application.to_dict() for application in Application.query.filter_by(user_id=id).all()]
+        if not applications:
+            return make_response({
+                "error":"Currently there are no applications"
+            },400)
+        
+        return make_response(applications,200)
+api.add_resource(Applications_by_user_id,'/applications/user/<int:id>')
+
+#Handle the shortlisting 
+#The employer should leave comments for the shortlisted employees
+# Add a table for comments which should have a relationship with the shortlisting since the employer can leave multiple comments
+
+    
+    
+# Running the app
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
